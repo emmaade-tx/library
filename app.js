6c128725d310a8bb50fed16d4a1fe2bdae98b049
Emmanuel Ade <emmade.tx@gmail.com>
2016-09-25 00:32:11 +0100

    add handlebars

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.set('views', './src/view');
-app.set('view engine', 'jade');
+
+const handlebars = require('express-handlebars');
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-	res.render('index', {list: ['a', 'b']});
+	res.render('index', {title: 'Hello from render', list: ['a', 'b']});
 });
 
 app.get('/book', (req, res) => {
